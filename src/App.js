22e13aa8c62cc4fb8877ed2a40682b38895b02ac
Julian Klett <julian22a@googlemail.com>
2023-11-13 15:34:21 +0100

    changed app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from "react";
 import "./styles.css";
 
+function Summation(a, b) {
+  let numberA = parseInt(a);
+  let numberB = parseInt(b);
+  const sum = numberA + numberB;
+  return sum;
+}
+
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Sum valueA="2" valueB="4" />;
+}
+
+function Sum({ valueA, valueB }) {
+  return (
+    <h1>
+      {valueA} + {valueB} = {Summation(valueA, valueB)}
+    </h1>
+  );
 }
